3245424dc13165c439bfc5fb35fb6bfba258d2da
Angus061855 <165797999+Angus061855@users.noreply.github.com>
2026-06-08 23:53:12 +0800

    Update post_3.py

--- post_3.py
+++ post_3.py
@@ -229,7 +229,7 @@ if __name__ == "__main__":
         post_to_threads(target_content)
         update_status(page_id, "已發")
         print("✅ 完成！")
-        send_telegram("✅ media 給文章 發文成功！")
+        send_telegram("✅ Thread White 3 給文章 發文成功！")
     except Exception as e:
         error_msg = f"❌ media 給文章 發文失敗！\n錯誤原因：{str(e)}"
         print(error_msg)
